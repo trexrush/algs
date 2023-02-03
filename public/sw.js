1c2bc9d6eb8f830cbc54009d80628eef23850944
Trexrush <edmazuera@gmail.com>
2023-02-02 20:11:58 -0600

    implement stale-while-revalidate

--- public/sw.js
+++ public/sw.js
@@ -1,40 +1,38 @@
-const version = '1'
+const version = '2'
 const CACHE = `cache-${version}`
 
-
 self.addEventListener('install', event => {
-    event.waitUntil((async () => {
-        // const cache = await caches.open(CACHE_NAME);
-        // cache.addAll(PRECACHE_ASSETS);
-        console.log('SW: Installing');
-
-    })());
+    console.log('SW: Installing');
+    event.waitUntil((async () => { /* static */ })());
 });
 
 self.addEventListener('activate', event => {
     console.log('SW: Activating');
-
-    // event.waitUntil(() => {
-    //     clients.claim()
-    // });
     self.clients.claim()
 });
 
+// https://jcs.wtf/service-worker-stale-while-revalidate/
+const fetchThenCache = async (event, cache) => {
+    const response = await fetch(event.request)
+    // https://stackoverflow.com/questions/49157622/service-worker-typeerror-when-opening-chrome-extension
+    if (!event.request.url.includes('extension')) { cache.put(event.request, response.clone()) }
+
+    console.log("SW: caching fetch ", response.url)
+    return response
+}
+
 self.addEventListener('fetch', event => {
-    
     event.respondWith((async () => {
         const cache = await caches.open(CACHE);
         const cachedResponse = await cache.match(event.request);
         
         if (cachedResponse) {
             console.log("SW: returning cached result ", cachedResponse.url)
+            fetchThenCache(event, cache)
             return cachedResponse;
         }
         
         console.log('SW: Cache not hit, fetching', event.request.url);
-        const response = await fetch(event.request)
-        // https://stackoverflow.com/questions/49157622/service-worker-typeerror-when-opening-chrome-extension
-        if (!event.request.url.includes('extension')) { cache.put(event.request, response.clone()) }
-        return response
+        return fetchThenCache(event, cache)
     })());
 });
