2f2c901dfa7ba0464713d3bdf08fa814fe3674ed
Trexrush <edmazuera@gmail.com>
2023-06-02 03:18:32 -0700

    refactor: TwistyPlayer to not define so much onMount

--- src/components/TwistyPlayer.svelte
+++ src/components/TwistyPlayer.svelte
@@ -11,19 +11,20 @@
   export let size: number
   export let isLefty: boolean = false
   let puzzle: twistyPuzzleType = puzzleDefinitionMapping[options.puzzle]?.type || DefaultOptions.puzzle
-  // bit of a hack
-  let puzzleChirality: twistyPuzzleType = options.puzzle == puzzle ? puzzle : options.puzzle
+  let puzzleChirality: twistyPuzzleType = options.puzzle == puzzle ? puzzle : options.puzzle  // bit of a hack
 
-  //TODO: export a twisty player (so make this a wrapper you import into AlgCard and AlgImage (with alg logic in there) rather than an element
-  
   export let setup = "";
   export let backView = false;
   export let hintFacelets = false;
   export let drag = false;
   let twistyDiv: HTMLDivElement
   
-  // canvas elements are undefined until load so this logic must be defined in onMount
-  let setAlg = (alg: string) => { return }
+  const setAlg = (alg: string) => {
+    twistyPlayer.cameraLongitude = isLefty ? -options.twistyplayerparams!.cameraX! : options.twistyplayerparams!.cameraX!
+    twistyPlayer.alg = ". . " + expandAlgWithTriggers(alg, puzzleChirality) + " . ."
+    twistyPlayer.jumpToStart()
+    twistyPlayer.controller.animationController.playPause()
+  }
   
   export let imageAlg: string | null = null
   export let activeAlg: string | null = null
@@ -33,43 +34,35 @@
     }
   }
 
+  const twistyPlayer: TwistyPlayer = new TwistyPlayer({
+    experimentalSetupAlg: setup + " " + options.twistyplayerparams!.rot!,
+    experimentalSetupAnchor: "end",
+    puzzle: options.puzzle,
+    visualization: imageAlg ? "experimental-2D-LL" : "3D",
+    hintFacelets: hintFacelets ? "auto" : "none",
+    experimentalStickering: options.twistyplayerparams!.stage!,
+    background: "none",
+    controlPanel: "none",
+    backView: backView ? "top-right" : "none",
+    experimentalDragInput: drag ? "auto" : "none",
+    cameraLatitude: options.twistyplayerparams!.cameraY!,
+    cameraLongitude: isLefty ? -options.twistyplayerparams!.cameraX! : options.twistyplayerparams!.cameraX!,
+    cameraDistance: puzzle == 'megaminx' ? 5.5 : 5,
+    cameraLatitudeLimit: 50,
+    tempoScale: options.twistyplayerparams!.tempo!,
+  });
+
+  if (imageAlg) { twistyPlayer.alg = expandAlgWithTriggers(imageAlg, puzzleChirality) }
+  twistyPlayer.experimentalFaceletScale = 0.88, // works here but not in vanillaJS??????
+  twistyPlayer.style.height = `${size}px`
+  twistyPlayer.style.width = `${size}px`
+
   onMount(async () => {
-      const twistyPlayer: TwistyPlayer = new TwistyPlayer({
-          experimentalSetupAlg: setup + " " + options.twistyplayerparams!.rot!,
-          experimentalSetupAnchor: "end",
-          puzzle: options.puzzle,
-          visualization: imageAlg ? "experimental-2D-LL" : "3D",
-          hintFacelets: hintFacelets ? "auto" : "none",
-          experimentalStickering: options.twistyplayerparams!.stage!,
-          background: "none",
-          controlPanel: "none",
-          backView: backView ? "top-right" : "none",
-          experimentalDragInput: drag ? "auto" : "none",
-          cameraLatitude: options.twistyplayerparams!.cameraY!,
-          cameraLongitude: isLefty ? -options.twistyplayerparams!.cameraX! : options.twistyplayerparams!.cameraX!,
-          cameraDistance: puzzle == 'megaminx' ? 5.5 : 5,
-          cameraLatitudeLimit: 50,
-          tempoScale: options.twistyplayerparams!.tempo!,
-        });
+    twistyDiv.appendChild(twistyPlayer);
 
-        if (imageAlg) { twistyPlayer.alg = expandAlgWithTriggers(imageAlg, puzzleChirality) }
-        twistyPlayer.experimentalFaceletScale = 0.88, // works here but not in vanillaJS??????
-        twistyPlayer.style.height = `${size}px`
-        twistyPlayer.style.width = `${size}px`
-
-    setAlg = (alg: string) => {
-        twistyPlayer.cameraLongitude = isLefty ? -options.twistyplayerparams!.cameraX! : options.twistyplayerparams!.cameraX!
-        twistyPlayer.alg = ". . " + expandAlgWithTriggers(alg, puzzleChirality) + " . ."
-        twistyPlayer.jumpToStart()
-        twistyPlayer.controller.animationController.playPause()
-    }
-      
-      twistyDiv.appendChild(twistyPlayer);
-      // console.log(twistyPlayer.experimentalModel.twistySceneModel.stickeringMask)
-
-      if (!imageAlg) {
-        twistyPlayer.play()
-        twistyPlayer.onclick = () => { twistyPlayer.controller.animationController.playPause(); }
+    if (!imageAlg) {
+      twistyPlayer.play()
+      twistyPlayer.onclick = () => { twistyPlayer.controller.animationController.playPause(); }
     }
   });
 
