6c9b25702e5b6c6caa550395b1b35a950084b995
Trexrush <edmazuera@gmail.com>
2023-07-04 02:09:06 -0700

    cleanup(algClass): annotate external uses of funcs

--- src/scripts/alg.ts
+++ src/scripts/alg.ts
@@ -3,6 +3,7 @@ import { backMoveGroups, baseMoveGroups, mirrorMoveGroups, triggerSubstitutionGr
 import type { IAlg, IOptions, ITrigger, TModifiersList, TNotationTargets, modularPuzzleGroup, twistyPuzzleType, twistyPuzzleTypeWithChirality } from "./types"
 import { cube3x3x3 } from "cubing/puzzles";
 
+// AlgImage, TwistyPlayer
 export const convert4x4Notation = (a: string, to: 'vc' | 'cubingjs'): string => {
   const notation = {
     display: ["r", "r'", "r2", "r2'", "l", "l'", "l2", "l2'", "M", "M'", "M2", "M2'"],
@@ -13,15 +14,11 @@ export const convert4x4Notation = (a: string, to: 'vc' | 'cubingjs'): string => 
     notation.display.includes(e) 
       ? notation[to][notation.display.indexOf(e)]
       : e
-    ).join(' ')
-
-// Disp PuzzleGen Cubingjs
-// r    Rw R'     2R      
-// M    M         m       
+    ).join(' ')    
 }
 
-// MOVABLE
-//TODO: implement swapping triggers
+// NO EXTERNAL but technically only called as the OverrideTriggers variant
+// TODO: implement swapping modifiers to work with triggers
 export const mirrorAlg = (a: string, pzl: twistyPuzzleTypeWithChirality ): string => {
   const baseMovesArray = baseMoveGroups[pzl]
   const mirrorMovesArray = mirrorMoveGroups[pzl]
@@ -33,13 +30,13 @@ export const mirrorAlg = (a: string, pzl: twistyPuzzleTypeWithChirality ): strin
   return aArr.join(" ")
 }
 
-// MOVABLE
+// AlgListing, CaseCard
 export const mirrorAlgOverrideTriggers = (a: string, pzl: twistyPuzzleTypeWithChirality) => {
   return mirrorAlg(expandAlgWithTriggers(a, pzl), pzl)
 }
 
-// MOVABLE
-export const backAlg = (a: string, pzl: keyof typeof baseMoveGroups ): string => {
+// NO EXTERNAL
+const backAlg = (a: string, pzl: keyof typeof baseMoveGroups ): string => {
   const baseMovesArray = baseMoveGroups[pzl]
   const backMovesArray = backMoveGroups[pzl]
   if (!baseMovesArray || !backMovesArray) { return '' }
@@ -50,21 +47,24 @@ export const backAlg = (a: string, pzl: keyof typeof baseMoveGroups ): string =>
   return aArr.join(" ")
 }
 
-// MOVABLE
-export const invertAlg = (a: string): string => {
+// NO EXTERNAL
+const invertAlg = (a: string): string => {
   return new Alg(a).invert().toString(); 
 }
 
-export const repeatAlg = (a: string, q: number, pzl: twistyPuzzleTypeWithChirality): string => {
+// NO EXTERNAL
+const repeatAlg = (a: string, q: number, pzl: twistyPuzzleTypeWithChirality): string => {
   let res = (a + " ").repeat(q)
   return simplifyAlg(res, pzl)
 }
 
-export const simplifyAlg = (a: string, pzl: twistyPuzzleTypeWithChirality): string => {
+// NO EXTERNAL
+const simplifyAlg = (a: string, pzl: twistyPuzzleTypeWithChirality): string => {
   return new Alg(a).experimentalSimplify({cancel: { directional: 'any-direction', puzzleSpecificModWrap: 'gravity' },
   puzzleSpecificSimplifyOptions: puzzleDefinitionMapping[pzl]?.cancel }).toString()
 }
 
+// AlgListing
 export const modifierActionsList: Record<TModifiersList, { action: (a: string, pzl: twistyPuzzleTypeWithChirality) => string, text: string }> = {
   "INVERSE": { action: (a, pzl) => { return invertAlg(a) }, text: 'INV'},
   "LEFTY": { action: (a, pzl) => { return mirrorAlgOverrideTriggers(a, pzl) }, text: 'LEFT'},
@@ -78,6 +78,7 @@ export const modifierActionsList: Record<TModifiersList, { action: (a: string, p
   "X3": { action: (a, pzl) => { return repeatAlg(a, 3, pzl) }, text: '3x'},
 }
 
+// NO EXTERNAL
 export const getTriggerAlg = (t: string, pzl: twistyPuzzleTypeWithChirality): string => {
   let triggerList: [...TModifiersList[], string] = t.split(' ') as [...TModifiersList[], string]
   // https://stackoverflow.com/questions/7574054/javascript-how-to-pass-object-by-value (how did I miss this)
@@ -94,21 +95,27 @@ export const getTriggerAlg = (t: string, pzl: twistyPuzzleTypeWithChirality): st
   return currTrigger.alg
 }
 
+// AlgListing
 export const isTriggerRegex: RegExp = /\[(.*?)\]/g
+// AlgListing
 // https://stackoverflow.com/questions/19414193/regex-extract-string-not-between-two-brackets
 export const algDelimiterWithTriggers: RegExp = /[\s\(\)]+(?![^\[]*\])/g
-export const matchAllParenthesis: RegExp =  /([\(\)])/g
+// in BackAlg, no External files
+const matchAllParenthesis: RegExp =  /([\(\)])/g
 
 // TODO: allow multiple layers of expanding algs (would allow definitions of triggers to use triggers)
+// AlgImage, AlgListing, TwistyPlayer
 export const expandAlgWithTriggers = (a: string, pzl: twistyPuzzleTypeWithChirality): string => {
   const getTriggerAlgWrapper = (b: string) => { return getTriggerAlg(b, pzl) } // curry away puzzle type to allow func call bind
   return simplifyAlg(a.replaceAll(isTriggerRegex, Function.prototype.call.bind(getTriggerAlgWrapper)), pzl)
 }
 
+// IMPLEMENTED, NOT NEEDED
 export const checkIfHasTriggers = (a: string): boolean => {
   return a.match(isTriggerRegex) != null ? true : false
 }
 
+// AlgImage (vc), AlgListing (mirror), TwistyPuzzle (type), CaseCard (standard / mirror)
 export const puzzleDefinitionMapping: modularPuzzleGroup<{ 
   type: twistyPuzzleType, 
   standard: twistyPuzzleTypeWithChirality, 
@@ -141,16 +148,12 @@ export const puzzleDefinitionMapping: modularPuzzleGroup<{
 
 // WIP REWRITE OF ALG LOGIC
 // TODO: implement Collection class that uses the generic data type thing and Symbol.iterator
-// https://dev.to/qgabe/functional-programming-design-patterns-part-1-strategy-pattern-4f92
-// https://leantechniques.com/2020/08/21/a-functional-approach-to-the-builder-pattern/
-
-// https://stackoverflow.com/questions/57828972/creating-a-fluent-stateful-builder-using-the-typescript-type-system
-// https://dev.to/padcom/type-safe-object-builder-in-ts-295e
 
 // move to types
 
 type TAlgCommon = Pick<IOptions, "puzzle" | "imgSource"> & {
   isMirror: boolean
+  puzzleMap: IPuzzleDefinitionMapping
   mirror: () => string
   invert: () => string
   backMirror: () => string
@@ -164,45 +167,37 @@ interface IAlgorithmClass extends IAlg, TAlgCommon {
 }
 interface ITriggerClass extends ITrigger, TAlgCommon {}
 
-type IPuzzleDefinitionMapping = modularPuzzleGroup<{ 
+type IPuzzleDefinitionMapping = { 
   type: twistyPuzzleType, 
-  standard: twistyPuzzleTypeWithChirality, 
-  mirror: twistyPuzzleTypeWithChirality, 
+  right: twistyPuzzleTypeWithChirality, 
+  left: twistyPuzzleTypeWithChirality, 
   vc: string | number, 
   cancel?: PuzzleSpecificSimplifyOptions,
   notation?: (a: string, to: "vc" | "cubingjs") => string,
-}>
+}
 
 // ___ //
 
 const AlgBuilder = () => { // TODO: take into account chirality
-  let algorithm: Partial<IAlgorithmClass & ITriggerClass> = {
-    isMirror: false,
-  }
-
-  const puzzleMapping: IPuzzleDefinitionMapping = {
+  const puzzleMapping: modularPuzzleGroup<IPuzzleDefinitionMapping> = {
     '3x3x3': { 
-      type: '3x3x3', standard: '3x3x3', mirror: '3x3x3', vc: 3, 
+      type: '3x3x3', right: '3x3x3', left: '3x3x3', vc: 3, 
       cancel: cube3x3x3.puzzleSpecificSimplifyOptions 
     },
     '4x4x4': { 
-      type: '4x4x4', standard: '4x4x4', mirror: '4x4x4', vc: 4, 
+      type: '4x4x4', right: '4x4x4', left: '4x4x4', vc: 4, 
       cancel: { quantumMoveOrder: () => 4 }, 
       notation: (a, to) => { return convert4x4Notation(a, to) }
     },
     '2x2x2': { 
-      type: '2x2x2', standard: '2x2x2', mirror: '2x2x2', vc: 2 
+      type: '2x2x2', right: '2x2x2', left: '2x2x2', vc: 2 
     },
     'megaminx': { 
-      type: 'megaminx', standard: 'megaminx', mirror: 'megaminx-lefty', vc: 'mega', 
-      cancel: { quantumMoveOrder: () => 5 } 
-    },
-    'megaminx-lefty': { 
-      type: 'megaminx', standard: 'megaminx-lefty', mirror: 'megaminx', vc: 'mega', 
+      type: 'megaminx', right: 'megaminx', left: 'megaminx-lefty', vc: 'mega', 
       cancel: { quantumMoveOrder: () => 5 } 
     },
   }
-
+  
   // TODO: localize
   const modifierActions: Record<TModifiersList, { action: (a: string, pzl: twistyPuzzleTypeWithChirality) => string, text: string }> = {
     "INVERSE": { action: (a, pzl) => { return invertAlg(a) }, text: 'INV'},
@@ -216,11 +211,15 @@ const AlgBuilder = () => { // TODO: take into account chirality
     "TRIPLE": { action: (a, pzl) => { return repeatAlg(a, 3, pzl) }, text: '3x'},
     "X3": { action: (a, pzl) => { return repeatAlg(a, 3, pzl) }, text: '3x'},
   }
+  
+  let algorithm: Partial<IAlgorithmClass & ITriggerClass> = {
+    isMirror: false,
+  }
 
   const builders = {
     stage1: {
       withPuzzle: (p: twistyPuzzleType) => {
-        algorithm = {...algorithm, puzzle: p }
+        algorithm = {...algorithm, puzzle: p, puzzleMap: puzzleMapping[p] }
         return builders.stage2
       }
     },
@@ -238,7 +237,7 @@ const AlgBuilder = () => { // TODO: take into account chirality
             return expandAlgWithTriggers(algorithm.alg!, algorithm.puzzle!)
           },
           notation: (to) => { return puzzleTraits?.notation ? puzzleTraits?.notation(algorithm.alg!, to) : algorithm.alg! },
-          mirror: () => { return mirrorAlg(algorithm.alg!, algorithm.puzzle!) },
+          mirror: () => { return mirrorAlgOverrideTriggers(algorithm.alg!, algorithm.puzzle!) },
           backMirror: () => { return backAlg(algorithm.alg!, algorithm.puzzle!) },
           invert: () => { return invertAlg(algorithm.alg!) },
           simplify: () => { return simplifyAlg(algorithm.alg!, algorithm.puzzle!) },
@@ -278,9 +277,10 @@ const AlgBuilder = () => { // TODO: take into account chirality
   return (() => { return builders.stage1 })()
 }
 
-// MANUAL TESTING
+// MANUAL TESTING 
+// TODO: unit tests
 
-let testData: IAlg = { alg: "R U R' U' R U2' R'" }
-let testTriggerData: string = "LEFT BACK SEXY"
-let testAlg: IAlgorithmClass = AlgBuilder().withPuzzle('3x3x3').withAlgData(testData).build() as IAlgorithmClass
-let testTrig: ITriggerClass = AlgBuilder().withPuzzle('3x3x3').withTrigger(testTriggerData).build() as ITriggerClass
+// let testData: IAlg = { alg: "R U R' U' R U2' R'" }
+// let testTriggerData: string = "LEFT BACK SEXY"
+// let testAlg: IAlgorithmClass = AlgBuilder().withPuzzle('3x3x3').withAlgData(testData).build() as IAlgorithmClass
+// let testTrig: ITriggerClass = AlgBuilder().withPuzzle('3x3x3').withTrigger(testTriggerData).build() as ITriggerClass
