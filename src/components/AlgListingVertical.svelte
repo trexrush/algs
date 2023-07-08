f4bd99152ad7f2e3f0cc10e55802edb194f5e987
Trexrush <edmazuera@gmail.com>
2023-07-07 23:27:23 -0700

    style: cleanup

--- src/components/AlgListingVertical.svelte
+++ src/components/AlgListingVertical.svelte
@@ -8,16 +8,22 @@
     button: `my-[2px] p-[1px] 
             sm:text-sm 
             rounded-md shadow-md cursor-pointer select-none`,
-  }
+  };
+  const localStyles = {
+    main: `hover:text-[2.95vw] sm:hover:text-[19px] transition-all duration-100 cursor-pointer group`,
+    left: `${isActive ? 'bg-gradient-to-r from-teal-500 via-green-500 to-lime-500' : 'text-lime-100/40'}`,
+    right: `${isActive ? 'bg-gradient-to-r from-red-500 via-orange-500 to-amber-500' : 'text-red-100/40'}`,
+    active: "text-[2.9vw] sm:text-[18.5px] font-semibold bg-clip-text text-transparent",
+    normal: "text-[2.8vw] sm:text-[17.5px] font-extralight",
+  };
   const Styles = {
     setup: { 
       main: `px-1 rounded-sm text-[2vw] sm:text-[13px] text-stone-500 font-bold bg-stone-200/[.07]` 
     },
     display: {
-      expandMove: `group-hover:text-stone-900 dark:group-hover:text-stone-100`,
-      expandTrigger: `group/trigger ${ isActive ? 'font-extrabold' : 'font-bold'}`,
-      expandTriggerBase: `${isActive ? 'group-hover:text-white/40' : algorithm.isLefty ? 'group-hover:text-lime-300' : 'group-hover:text-red-300'}`,
-      expandMods: `mx-[1px] text-[1.5vw] sm:text-[10px] text-stone-500 transition-all duration-100 group-hover:text-[1.6vw] group-hover:sm:text-[10.7px] 
+      triggerGroup: `group/trigger ${ isActive ? 'font-extrabold' : 'font-bold'}`,
+      triggerBase: `${isActive ? 'group-hover:text-white/40' : algorithm.isLefty ? 'group-hover:text-lime-300' : 'group-hover:text-red-300'}`,
+      mods: `mx-[1px] text-[1.5vw] sm:text-[10px] text-stone-500 transition-all duration-100 group-hover:text-[1.6vw] group-hover:sm:text-[10.7px] 
       ${algorithm.isLefty ? 'group-hover/trigger:text-lime-100' : 'group-hover/trigger:text-red-100'}`,
       alg: `group-hover:text-stone-900 dark:group-hover:text-stone-100`,
     },
@@ -33,13 +39,7 @@
       transition-colors duration-75`
     },
   };
-  const localStyles = {
-    main: `hover:text-[2.95vw] sm:hover:text-[19px] transition-all duration-100 cursor-pointer group`,
-    left: `${isActive ? 'bg-gradient-to-r from-teal-500 via-green-500 to-lime-500' : 'text-lime-100/40'}`,
-    right: `${isActive ? 'bg-gradient-to-r from-red-500 via-orange-500 to-amber-500' : 'text-red-100/40'}`,
-    active: "text-[2.9vw] sm:text-[18.5px] font-semibold bg-clip-text text-transparent",
-    normal: "text-[2.8vw] sm:text-[17.5px] font-extralight",
-  };
+
 </script>
 
 <slot name="play" css={Styles.play}/>
