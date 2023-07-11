1a601b58a60b818838fe2c7d49f13ee4dedea45c
Trexrush <edmazuera@gmail.com>
2023-07-11 01:49:01 -0700

    config: cleanup and postcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import yaml from '@rollup/plugin-yaml'
 
-// import AstroPWA from '@vite-pwa/astro'
-// import { VitePWA } from "vite-plugin-pwa"
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
