67d440c5de62309db5f5307b744ebb3795dbc216
Trexrush <edmazuera@gmail.com>
2023-07-23 12:57:09 -0700

    cleanup: remove unused utilities

--- src/scripts/utilities.ts
+++ src/scripts/utilities.ts
@@ -2,23 +2,6 @@ import tippy from 'tippy.js';
 import 'tippy.js/animations/scale-subtle.css';
 import '../../public/basic-tooltip.css'
 
-// https://svelte.school/tutorials/how-to-use-variables-in-style-tags-in-svelte
-export const styles = (node: any, styles: any) => {
-	setCustomProperties(node, styles)
-	
-	return {
-		update(styles: any) {
-			setCustomProperties(node, styles)
-		}
-	};
-}
-
-function setCustomProperties(node: { style: { setProperty: (arg0: string, arg1: unknown) => void } }, styles: { [s: string]: unknown } | ArrayLike<unknown>) {
-	Object.entries(styles).forEach(([key, value]) => {
-		node.style.setProperty(`--${key}`, value)
-	})
-}
-
 // https://dev.to/danawoodman/svelte-quick-tip-using-actions-to-integrate-with-javascript-libraries-tippy-tooltips-2m94
 export function tooltip(node: HTMLElement, params: any = {}) {
   // Determine the title to show. We want to prefer the custom content passed in first, then the
