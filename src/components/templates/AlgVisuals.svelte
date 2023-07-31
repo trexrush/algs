837f83bfcca76fa712af632121274553e62f37b8
Trexrush <edmazuera@gmail.com>
2023-07-31 00:03:52 -0700

    feat/data: finish T ZBLL

--- src/components/templates/AlgVisuals.svelte
+++ src/components/templates/AlgVisuals.svelte
@@ -61,6 +61,7 @@
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 
 <svelte:component this={Layout} {toggleDisplay} {isAlgVisDisplayed}>
   <svelte:fragment slot="algAnimation" let:css>
