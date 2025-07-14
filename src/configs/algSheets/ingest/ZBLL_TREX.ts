306d105725310262499241ad49f46d16a35e2eda
Trexrush <edmazuera@gmail.com>
2025-07-14 02:12:10 -0700

    clean: rename zbll data files so they show up together

--- src/configs/algSheets/ingest/ZBLL_TREX.ts
+++ src/configs/algSheets/ingest/ZBLL_TREX.ts
@@ -1,8 +1,8 @@
 import { useZBLLGrouping } from "../../setConstants/setGroupings/zbll";
 import type { ICaseList } from "../../../types/dataFormat"
 
-import t from '../data/T_ZBLL_TREX.yml'
-import u from '../data/U_ZBLL_TREX.yml'
+import t from '../data/ZBLL_T_TREX.yml'
+import u from '../data/ZBLL_U_TREX.yml'
 
 export const zbllTrexSheetGroupings = useZBLLGrouping({
   ...t as ICaseList, 
